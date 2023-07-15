34807069af6718275cbdf9c1fc1033428eaef815
nehiro <hiroaki.nerio@gmail.com>
2023-07-15 11:46:24 +0900

    first push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import NavBar from './components/nav-bar'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <NavBar></NavBar>
+        {children}
+      </body>
     </html>
   )
 }
